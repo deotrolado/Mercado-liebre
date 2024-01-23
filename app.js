6a8708c8064b666116d95d3f913a1754d0d80e4c
deotrolado <facundomatias642@gmail.com>
2024-01-23 17:17:26 -0300

    paso1

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+//aca estoy importando las cosas
 const express = require('express');
+const path = require ("path") ;
 
+// aca cree expres
 const app = express();
+
+
+// aca esta el root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+//el 404
+app.get('*', (req, res) => {
+    res.status(404).send("404 - Not Found");
+  });
+//
 const port = process.env.port || 3000;
 app.listen (port, () => console.log ("servidor andando"));
 
 //los parametros de la funcion son un callback
 
 app.get ("/", function (req, res) {
-    res.send ("llegaste al sitio")
+    res.send ("llegaste al sitio2")
 });
